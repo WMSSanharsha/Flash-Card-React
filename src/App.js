e8746e3151071f2d67bb0695b2b52ceb4fe01d0a
Sakila Sanharsha <sakila.sanharsha2k19@gmail.com>
2024-09-29 13:37:13 +0530

    flash card final version

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import "./App.css";
 
 export default function App() {
@@ -43,11 +44,22 @@ const questions = [
 ];
 
 function FlashCards() {
+  const [selectedId, setSelectedId] = useState(null);
+
+  function handleClick(id) {
+    setSelectedId(id !== selectedId ? id : null);
+  }
   return (
-    <div>
+    <div className="flashcards">
       {questions.map((question) => (
-        <div>
-          <p>{question.question}</p>
+        <div
+          key={question.id}
+          onClick={() => handleClick(question.id)}
+          className={question.id === selectedId ? "selected" : ""}
+        >
+          <p>
+            {question.id === selectedId ? question.answer : question.question}
+          </p>
         </div>
       ))}
     </div>
